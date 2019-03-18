e8d8e84d73d9937c650294175a6838befa3effda
Hông Duc <hong-duc.ta@protonmail.com>
2019-03-18 23:17:03 +0100

    feat: Began Exercice 4

--- src/main/java/WebSemantic/Jena4.java
+++ src/main/java/WebSemantic/Jena4.java
@@ -1,4 +1,52 @@
 package WebSemantic;
 
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Jena4 {
+
+    public void main() throws IOException
+    {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter the name of the movie : ");
+        String title = reader.nextLine();
+        System.out.print(title);
+
+        Model model = ModelFactory.createDefaultModel();
+        String filePath = new File("").getAbsolutePath();
+        // read the TTL file
+        model.read(filePath + "\\src\\main\\data\\final.ttl", null);
+
+        String queryString =
+                "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                        "PREFIX owl: <http://www.w3.org/2002/07/owl#> " +
+                        "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#> " +
+                        "PREFIX : <http://www.semanticweb.org/julian/ontologies/2019/2/untitled-ontology-3#>" +
+                        "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> " +
+                        "SELECT ?year ?country ?genre ?actors " +
+                        "WHERE { " +
+                        "?x rdf:type owl:NamedIndividual ." +
+                        "?x :hasTitle \"" + title +"\" ." +
+                        "?x :hasCountry ?country ." +
+                        "?x :hasYear ?year ." +
+                        "?x :hasGenre ?genre ." +
+                        "?x :hasActor ?actors}";
+
+        Query query = QueryFactory.create(queryString);
+
+        QueryExecution qe = QueryExecutionFactory.create(query,model);
+        ResultSet results = qe.execSelect();
+
+        ResultSetFormatter.out(System.out,results,query);
+
+        qe.close();
+
+        System.out.println("\nFin Exercice 4\n");
+
+    }
 }
